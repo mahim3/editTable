3baee4a9de6bafc2b24a42ebd3aa96b69ab0060b
mahim3 <mahespappini7@gmail.com>
2018-02-19 18:13:46 +0530

    Modified files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+ tableData: any =[];
+ showEditTable:boolean = false;
+ editRowID: any = '';
+
+ constructor(){
+   this.tableData= [
+     {id:1,name: 'Mahi', mobile:'456465' , email:'mahi@gmail.com'},
+     {id:2,name: 'Alice', mobile:'458765' , email:'Alice@gmail.com'},
+     {id:3,name: 'Bob', mobile:'456448' , email:'Bob@gmail.com'}
+   ]
+ }
+
+ Edit(val){
+   this.editRowID= val;
+ }
 }
